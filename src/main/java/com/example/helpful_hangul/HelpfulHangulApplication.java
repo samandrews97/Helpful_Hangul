115af7282761ed43e73de8054afe172e830b6ca6
samandrews97 <sam@asandrews.co.uk>
2026-09-08 16:19:59 +0100

    Initial Commit

---
+++ src/main/java/com/example/helpful_hangul/HelpfulHangulApplication.java
@@ -0,0 +1,13 @@
+package com.example.helpful_hangul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpfulHangulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpfulHangulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helpful_hangul/HelpfulHangulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helpful_hangul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpfulHangulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
